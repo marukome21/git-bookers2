142ac401b7b2fef965a859aa5ddd92fc9c4ae963
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-23 17:05:18 +0000

    [Add] user model カラム　book model カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_23_161839) do
+ActiveRecord::Schema.define(version: 2024_04_23_165854) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2024_04_23_161839) do
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,6 +56,8 @@ ActiveRecord::Schema.define(version: 2024_04_23_161839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
