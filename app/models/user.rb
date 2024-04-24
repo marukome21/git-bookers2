d008a7b7bf60f0862c1d91ff65b866ae20b60777
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-24 08:14:01 +0000

    [Add] User model と Book model のアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-   has_one_attached :profile_image
+         
+  has_many :books, dependent: :destroy
+         
+  has_one_attached :profile_image
   
   def get_profile_image(width, height)
   unless profile_image.attached?
@@ -12,6 +15,6 @@ class User < ApplicationRecord
     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
-end
+  end
   
 end
