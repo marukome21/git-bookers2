086f852cd9612251f4a810c7105b4e99ddec3ad0
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-04-25 13:33:40 +0000

    [Add] ユーザーindex,show,update ブックedit,update,destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,25 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
   end
 
   def index
-
+    @book = Book.new
+    @user = User.find(current_user.id)
+    @users = User.all
   end
 
 
